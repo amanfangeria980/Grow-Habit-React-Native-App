df9c4a896ffa7147349ba2840f749e009fb7f2d1
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-21 13:30:30 +0530

    minor changes

--- app/(drawer)/(tabs)/Home.tsx
+++ app/(drawer)/(tabs)/Home.tsx
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 import React from "react";
 
-const Dashboard = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Dashboard</Text>
+      <Text>Home</Text>
     </View>
   );
 };
 
-export default Dashboard;
+export default Home;
