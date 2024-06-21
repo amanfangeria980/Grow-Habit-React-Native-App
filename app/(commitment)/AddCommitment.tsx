9ed57975b05766f3de02e094718d41280d61a678
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-21 19:02:42 +0530

    Made a basic commitment form using yup and formik

--- app/(commitment)/AddCommitment.tsx
+++ app/(commitment)/AddCommitment.tsx
@@ -1,13 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
+import CommitmentForm from "./../components/CommitmentForm";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 const AddCommitment = () => {
   return (
-    <View className="flex-1 bg-gray-200">
-      <Text className="text-3xl mx-auto mt-2 text-orange-500 font-bold underline">
-        Let's Commit...
-      </Text>
-    </View>
+    <ScrollView className="flex-1 bg-gray-200 relative">
+      <TouchableOpacity
+        className="absolute right-2 top-2 "
+        onPress={() => router.back()}
+      >
+        <MaterialIcons name="cancel" size={60} color="red" />
+      </TouchableOpacity>
+      <View className="flex-1 mt-10">
+        <Text className="text-3xl mx-auto mb-5 text-orange-500 font-bold underline">
+          Let's Commit...
+        </Text>
+        <CommitmentForm />
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ app/components/CommitmentForm.tsx
@@ -0,0 +1,126 @@
+// Formik x React Native example
+import React from "react";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import { Formik } from "formik";
+import { router } from "expo-router";
+import { object, string, number, date } from "yup";
+
+let commitmentSchema = object({
+  habitName: string().required("Please enter Habit Name!"),
+  habitDescription: string().required("Please enter Habit Description!"),
+  gatewayStatement: string().required("Please enter Gateway Statement!"),
+  plusStatement: string().required("Please enter Plus Statement!"),
+  eliteStatement: string().required("Please enter Elite Statement!"),
+  // createdOn: date().default(() => new Date()),
+});
+
+const CommitmentForm = (props) => (
+  <Formik
+    initialValues={{
+      habitName: "",
+      habitDescription: "",
+      gatewayStatement: "",
+      plusStatement: "",
+      eliteStatement: "",
+    }}
+    onSubmit={(values) => {
+      console.log(JSON.stringify(values, null, 2));
+      router.back();
+    }}
+    validationSchema={commitmentSchema}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+      <View className="mx-2 mb-8">
+        <View className="mb-5">
+          <Text className="text-xl mb-2">🎯 Habit Name: </Text>
+          <TextInput
+            onChangeText={handleChange("habitName")}
+            onBlur={handleBlur("habitName")}
+            value={values.habitName}
+            className="border border-orange-500 max-h-24 text-xl p-2"
+            multiline={true}
+            textAlignVertical="top"
+          />
+          {errors.habitName && (
+            <Text className="text-red-500 mt-1 text-lg">
+              {errors.habitName}
+            </Text>
+          )}
+        </View>
+        <View className="mb-5">
+          <Text className="text-xl mb-2">📋 Habit Description: </Text>
+          <TextInput
+            onChangeText={handleChange("habitDescription")}
+            onBlur={handleBlur("habitDescription")}
+            value={values.habitDescription}
+            className="border border-orange-500 h-50 text-xl p-2"
+            multiline={true}
+            textAlignVertical="top"
+          />
+          {errors.habitDescription && (
+            <Text className="text-red-500 mt-1 text-lg">
+              {errors.habitDescription}
+            </Text>
+          )}
+        </View>
+        <View className="mb-5">
+          <Text className="text-xl mb-2">Gateway Statement (🐤): </Text>
+          <TextInput
+            onChangeText={handleChange("gatewayStatement")}
+            onBlur={handleBlur("gatewayStatement")}
+            value={values.gatewayStatement}
+            className="border border-orange-500 max-h-24 text-xl p-2"
+            multiline={true}
+            textAlignVertical="top"
+          />
+          {errors.gatewayStatement && (
+            <Text className="text-red-500 mt-1 text-lg">
+              {errors.gatewayStatement}
+            </Text>
+          )}
+        </View>
+        <View className="mb-5">
+          <Text className="text-xl mb-2">Plus Statement (⭐): </Text>
+          <TextInput
+            onChangeText={handleChange("plusStatement")}
+            onBlur={handleBlur("plusStatement")}
+            value={values.plusStatement}
+            className="border border-orange-500 max-h-24 text-xl p-2"
+            multiline={true}
+            textAlignVertical="top"
+          />
+          {errors.plusStatement && (
+            <Text className="text-red-500 mt-1 text-lg">
+              {errors.plusStatement}
+            </Text>
+          )}
+        </View>
+        <View className="mb-5">
+          <Text className="text-xl mb-2">Elite Statement (🌟): </Text>
+          <TextInput
+            onChangeText={handleChange("eliteStatement")}
+            onBlur={handleBlur("eliteStatement")}
+            value={values.eliteStatement}
+            className="border border-orange-500 max-h-24 text-xl p-2"
+            multiline={true}
+            textAlignVertical="top"
+          />
+          {errors.eliteStatement && (
+            <Text className="text-red-500 mt-1 text-lg">
+              {errors.eliteStatement}
+            </Text>
+          )}
+        </View>
+        <View className="mt-2">
+          <TouchableOpacity
+            onPress={handleSubmit}
+            className="border-2  bg-orange-500 w-40 mx-auto p-4 rounded-lg"
+          >
+            <Text className="text-center text-xl">Commit 🙆</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )}
+  </Formik>
+);
+export default CommitmentForm;
