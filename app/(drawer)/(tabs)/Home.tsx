5cbfdcc5c3475e2c54e6a47d3c2b277f5e330c83
Aman Fangeria <aman.fangeria@geekyants.com>
2024-08-02 17:05:53 +0530

    Fetched commitments and display the task description on home page

--- app/(drawer)/(tabs)/Home.tsx
+++ app/(drawer)/(tabs)/Home.tsx
@@ -1,17 +1,36 @@
 import { SafeAreaView, ScrollView, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DailyReflectionForm from "../../components/DailyReflectionForm";
+import { fetchCommitments } from "../../../utils/functions";
+import { useUserContext } from "../../../context/UserContext";
 
 const Home = () => {
-  return (
-    <ScrollView className="flex-1">
-      <View className="flex-1">
-        <Text className="mx-auto font-bold text-2xl mt-2">Today's Task</Text>
-        <Text className="mx-auto font-bold text-2xl mt-2">Task Title</Text>
-        <DailyReflectionForm />
-      </View>
-    </ScrollView>
-  );
+    const [commitments, setCommitments] = useState([]);
+    const { user } = useUserContext();
+    useEffect(() => {
+        const fetchCommitmentsData = async () => {
+            try {
+                const commitmentsData = await fetchCommitments(user.id);
+                setCommitments(commitmentsData);
+            } catch (error) {
+                console.error("Error fetching commitments:", error);
+            }
+        };
+        fetchCommitmentsData();
+    }, []);
+    return (
+        <ScrollView className="flex-1">
+            <View className="flex-1">
+                <Text className="mx-auto font-bold text-2xl mt-2">
+                    Today's Task
+                </Text>
+                <Text className="mx-auto font-bold text-2xl mt-2">
+                    {commitments[commitments.length - 1]?.habitDescription}
+                </Text>
+                <DailyReflectionForm />
+            </View>
+        </ScrollView>
+    );
 };
 
 export default Home;
