8c10844824fa6190862fb8b9d8122b6f030e6c60
amanfangeria980 <amanfangeria980@gmail.com>
2024-08-02 11:35:06 +0530

    some changes added

---
+++ app/(tips)/tips.tsx
@@ -0,0 +1,13 @@
+// COC Prompts : ( Questions you might want to ask your comrade )
+// => What would be the happiest moment of your life ?
+// => On your deathbed what would you want to be written ?
+// => What's that one thing that you want yourself to hold accountable for ?
+// => What's your favourite food ?
+// => What's one of your favourite hobby ?
+// => Who is the person who has most inspired you in your life ?
+
+// Reflection Prompts :
+// How did your habit go today ?
+// If it didn't go the way it should, what was the reason ?
+// Anything super cool/ super frustrating happened today
+// Just wanna do a brain dump ! It's the place.
