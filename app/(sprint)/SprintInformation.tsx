0460639314fa7797af6c8451a6da483694bbdc04
Aman Fangeria <aman.fangeria@geekyants.com>
2024-08-07 20:39:26 +0530

    Sprint join done

--- app/(sprint)/SprintInformation.tsx
+++ app/(sprint)/SprintInformation.tsx
@@ -1,22 +1,44 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { Entypo, Ionicons } from "@expo/vector-icons";
-
+import { getMonthUtil } from "../../utils/functions";
+import { useUserContext } from "../../context/UserContext";
+import firestore, { arrayUnion } from "@react-native-firebase/firestore";
 const SprintInformation = () => {
     const { data } = useLocalSearchParams();
-
-    // Deserialize the data
     const sprintData = JSON.parse(data);
+    const { user } = useUserContext();
+    const date = new Date(sprintData.createdOn.seconds * 1000);
+    const month = getMonthUtil(date.getMonth() + 1).name;
 
     const handleInvite = () => {
         // Add your invite logic here
         console.log("Invite button pressed");
     };
 
-    const handleJoin = () => {
-        // Add your join logic here
-        console.log("Join button pressed");
+    const handleJoin = async () => {
+        try {
+            // Update the user's document in Firestore
+            await firestore()
+                .collection("Users")
+                .doc(user.id)
+                .update({
+                    sprintsJoined: firestore.FieldValue.arrayUnion({
+                        sprintId: sprintData.id,
+                        joinedOn: new Date(),
+                    }),
+                });
+
+            Alert.alert(
+                "Success",
+                "You have joined the sprint!",
+                [{ text: "OK" }] // Button text
+            );
+            router.push("/Sprint");
+        } catch (error) {
+            console.error("Error joining sprint: ", error);
+        }
     };
 
     return (
@@ -36,6 +58,9 @@ const SprintInformation = () => {
                 </Text>
                 <Text className="text-base mb-4">
                     {sprintData.membersJoined.length}
+                </Text>
+                <Text className="text-xl font-semibold mb-1 underline">
+                    Starts from {month}!
                 </Text>
             </View>
             <View className="flex-row justify-around">
